cace223699f32788bb77876b3eb40c8f44893c7e
Jinho Kim <wlsghrla94@gmail.com>
2022-02-26 18:27:22 +0900

    (0226)로그인, 회원가입

--- 01.project/js/login.js
+++ 01.project/js/login.js
@@ -1,12 +1,30 @@
+$(document).ready(function () {
 
-// const login = document.querySelector('#login');
-// login.addEventListener('click', function(evt) {
-//     const myid = document.querySelector('input[name="myid"]')
-//     if(myid.value == '') {
-//         evt.preventDefault();
-//         alert('아이디를 입력해주세요');
-//         myid.focus();
-//         myid.value = '';
-//         return false;
-//     }
-// })
+  // lastname 유효성 검사
+  $('#input-section button[name="login-button"]').click(function(evt) {
+
+    var myid = $('#input-section input[name="id-text-field"]')
+    var mypw = $('#input-section input[name="password-text-field"]')
+    // console.log(myid.val());
+    // console.log(mypw.val());
+    if(!myid.val()) {
+      $('span[title="myid-value"]').show().html('아이디를 입력하시오.').css('color', 'red')
+      myid.removeClass('is-valid')
+      myid.addClass('is-invalid')
+      evt.preventDefault();
+      myid.focus();
+    } else if (!mypw.val()){
+      $('span[title="myid-value"]').show().html('비밀번호를 입력하시오.').css('color', 'red')
+      mypw.removeClass('is-valid')
+      mypw.addClass('is-invalid')
+      evt.preventDefault();
+      mypw.focus();
+      // last.value = '';
+    }
+  });
+
+  
+
+
+
+});
